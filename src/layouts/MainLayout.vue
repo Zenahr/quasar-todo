62281d0aabb20fd58cfb5501c97d2b6171672f14
Zenahr <zenmatica@gmail.com>
2020-10-25 17:21:18 +0100

    update routes

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,6 +1,6 @@
 <template>
   <q-layout view="lHh Lpr lFf">
-    <q-header >
+    <q-header>
       <q-toolbar>
         <q-btn
           flat
@@ -12,130 +12,136 @@
         />
       </q-toolbar>
 
-<div class="q-px-lg q-pt-xl q-mb-md">
-  <div class="text-h3">Todo</div>
-  <div class="text-subtitle1">{{ currentDate }}</div>
-  <q-img src="../assets/bg.jpg" class="header-image absolute-top"/>
-</div>
+      <div class="q-px-lg q-pt-xl q-mb-md">
+        <div class="text-h3">Todo</div>
+        <div class="text-subtitle1">{{ currentDate }}</div>
+        <q-img src="../assets/bg.jpg" class="header-image absolute-top" />
+      </div>
     </q-header>
 
-     <q-drawer
-        v-model="leftDrawerOpen"
+    <q-drawer
+      v-model="leftDrawerOpen"
       show-if-above
       :width="300"
       :breakpoint="600"
       content-class="bg-grey-1"
+    >
+      <q-scroll-area
+        class="dark"
+        style="
+          height: calc(100% - 150px);
+          margin-top: 150px;
+          border-right: 1px solid #ddd;
+        "
       >
-        <q-scroll-area class="dark" style="height: calc(100% - 150px); margin-top: 150px; border-right: 1px solid #ddd">
-          <q-list padding>
-            
-            <q-item clickable v-ripple>
-              <q-item-section avatar>
-                <q-icon name="list" />
-              </q-item-section>
+        <q-list padding>
+          <q-item clickable v-ripple>
+            <q-item-section avatar>
+              <q-icon name="list" />
+            </q-item-section>
 
-              <q-item-section>
-                Todo
-              </q-item-section>
-            </q-item>
+            <q-item-section> Todo </q-item-section>
+          </q-item>
 
-            <q-item clickable v-ripple>
-              <q-item-section avatar>
-                <q-icon name="help" />
-              </q-item-section>
+          <q-item clickable v-ripple>
+            <q-item-section avatar>
+              <q-icon name="help" />
+            </q-item-section>
 
-              <q-item-section>
-                Help
-              </q-item-section>
-            </q-item>
-          </q-list>
-        </q-scroll-area>
+            <q-item-section> Help </q-item-section>
+          </q-item>
+        </q-list>
+      </q-scroll-area>
 
-        <q-img class="absolute-top" src="../assets/drawer_bg.jpg" style="height: 150px">
-          <div class="absolute-bottom bg-transparent">
-            <q-avatar size="56px" class="q-mb-sm">
-              <img src="../assets/avatar.jpg">
-            </q-avatar>
-            <div class="text-weight-bold">Zenahr Barzani</div>
-            <div>@Zenahr</div>
-          </div>
-        </q-img>
-      </q-drawer>
+      <q-img
+        class="absolute-top"
+        src="../assets/drawer_bg.jpg"
+        style="height: 150px"
+      >
+        <div class="absolute-bottom bg-transparent">
+          <q-avatar size="56px" class="q-mb-sm">
+            <img src="../assets/avatar.jpg" />
+          </q-avatar>
+          <div class="text-weight-bold">Zenahr Barzani</div>
+          <div>@Zenahr</div>
+        </div>
+      </q-img>
+    </q-drawer>
 
     <q-page-container class="dark">
-      <router-view />
+      <keep-alive>
+        <router-view />
+      </keep-alive>
     </q-page-container>
   </q-layout>
 </template>
 
 <script>
-import EssentialLink from 'components/EssentialLink.vue'
-import { date } from 'quasar'
-
+import EssentialLink from "components/EssentialLink.vue";
+import { date } from "quasar";
 
 const linksData = [
   {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev'
+    title: "Docs",
+    caption: "quasar.dev",
+    icon: "school",
+    link: "https://quasar.dev",
   },
   {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework'
+    title: "Github",
+    caption: "github.com/quasarframework",
+    icon: "code",
+    link: "https://github.com/quasarframework",
   },
   {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev'
+    title: "Discord Chat Channel",
+    caption: "chat.quasar.dev",
+    icon: "chat",
+    link: "https://chat.quasar.dev",
   },
   {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
+    title: "Forum",
+    caption: "forum.quasar.dev",
+    icon: "record_voice_over",
+    link: "https://forum.quasar.dev",
   },
   {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
+    title: "Twitter",
+    caption: "@quasarframework",
+    icon: "rss_feed",
+    link: "https://twitter.quasar.dev",
   },
   {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev'
+    title: "Facebook",
+    caption: "@QuasarFramework",
+    icon: "public",
+    link: "https://facebook.quasar.dev",
   },
   {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
-  }
+    title: "Quasar Awesome",
+    caption: "Community Quasar projects",
+    icon: "favorite",
+    link: "https://awesome.quasar.dev",
+  },
 ];
 
-
 export default {
-  name: 'MainLayout',
+  name: "MainLayout",
   components: { EssentialLink },
-  data () {
+  data() {
     return {
       leftDrawerOpen: false,
-      essentialLinks: linksData
-    }
+      essentialLinks: linksData,
+    };
   },
   computed: {
     currentDate() {
-      let timeStamp       = Date.now()
-      let formattedString = date.formatDate(timeStamp, 'dddd, Do MMMM YYYY')
-      return formattedString
-    }
-  }
-}
+      let timeStamp = Date.now();
+      let formattedString = date.formatDate(timeStamp, "dddd, Do MMMM YYYY");
+      return formattedString;
+    },
+  },
+};
 </script>
 
 
@@ -145,11 +151,10 @@ export default {
   color: white;
 }
 
-
 .header-image {
   height: 100%;
   z-index: -1;
-  opacity: .2;
+  opacity: 0.2;
   filter: grayscale(100%);
 }
 </style>
