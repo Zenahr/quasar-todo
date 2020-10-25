69b00f6b6aee0d98845e3841acb6684fe5d89ef6
Zenahr <zenmatica@gmail.com>
2020-10-25 17:19:04 +0100

    add dark theme

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -17,60 +17,41 @@
   <div class="text-subtitle1">{{ currentDate }}</div>
   <q-img src="../assets/bg.jpg" class="header-image absolute-top"/>
 </div>
-
     </q-header>
 
      <q-drawer
         v-model="leftDrawerOpen"
       show-if-above
-      bordered
+      :width="300"
+      :breakpoint="600"
       content-class="bg-grey-1"
       >
-        <q-scroll-area style="height: calc(100% - 150px); margin-top: 150px; border-right: 1px solid #ddd">
+        <q-scroll-area class="dark" style="height: calc(100% - 150px); margin-top: 150px; border-right: 1px solid #ddd">
           <q-list padding>
+            
             <q-item clickable v-ripple>
               <q-item-section avatar>
-                <q-icon name="inbox" />
+                <q-icon name="list" />
               </q-item-section>
 
               <q-item-section>
-                Inbox
-              </q-item-section>
-            </q-item>
-
-            <q-item active clickable v-ripple>
-              <q-item-section avatar>
-                <q-icon name="star" />
-              </q-item-section>
-
-              <q-item-section>
-                Star
+                Todo
               </q-item-section>
             </q-item>
 
             <q-item clickable v-ripple>
               <q-item-section avatar>
-                <q-icon name="send" />
+                <q-icon name="help" />
               </q-item-section>
 
               <q-item-section>
-                Send
-              </q-item-section>
-            </q-item>
-
-            <q-item clickable v-ripple>
-              <q-item-section avatar>
-                <q-icon name="drafts" />
-              </q-item-section>
-
-              <q-item-section>
-                Drafts
+                Help
               </q-item-section>
             </q-item>
           </q-list>
         </q-scroll-area>
 
-        <q-img class="absolute-top" src="https://cdn.quasar.dev/img/material.png" style="height: 150px">
+        <q-img class="absolute-top" src="../assets/drawer_bg.jpg" style="height: 150px">
           <div class="absolute-bottom bg-transparent">
             <q-avatar size="56px" class="q-mb-sm">
               <img src="../assets/avatar.jpg">
@@ -81,7 +62,7 @@
         </q-img>
       </q-drawer>
 
-    <q-page-container>
+    <q-page-container class="dark">
       <router-view />
     </q-page-container>
   </q-layout>
@@ -159,6 +140,12 @@ export default {
 
 
 <style lang="scss">
+.dark {
+  background-color: #131313;
+  color: white;
+}
+
+
 .header-image {
   height: 100%;
   z-index: -1;
